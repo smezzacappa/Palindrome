e66ff2eb9f191ecf5de99d66860fb872470a20fc
Steven <stevenmezzacappa@Stevens-MacBook-Pro.local>
2020-04-25 01:37:16 -0400

    Palindrome Practice

--- isPal.js
+++ isPal.js
@@ -3,12 +3,17 @@ function isPal(word) {
     //    //check to see if word is split into object array
     console.log(check)
 
+    //checks to see the length of the array
+    console.log(check.length)
+
     //     //starts a loop to check each letter in array 
     //because the string must have an even number of letters, you only need to run the loop as times as half the numbers in the word
     for (let i = 0; i < check.length / 2; i++) {
+
         //i starts at the first letter in the array 'check' and checks if it is equal to last letter
         //check.length = length of check array - 1 - number that i is itirated at
         if (check[i] != check[check.length - 1 - i]) {
+            
             //if not equal it returns false
             return false;
         }
@@ -17,4 +22,4 @@ function isPal(word) {
     }
 
 }
-isPal("wooeesseeoow")
+isPal("hannah")
